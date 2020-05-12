25327dcbacd239f44824625a2577c3b211790334
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-13 00:25:44 +0100

    add gamescreen component

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,10 +1,49 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const GameScreen = (props) => {
-  return <View></View>;
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randumNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randumNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+  return randumNumber;
 };
 
-const styles = StyleSheet.create({});
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  return (
+    <View style={styles.screen}>
+      <Text>Opponents guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 
 export default GameScreen;
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -14,7 +14,7 @@ import Input from "../components/Input";
 import Colors from "../constants/colors";
 import NumberContainer from "../components/NumberContainer";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onStartGame }) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
@@ -40,7 +40,7 @@ const StartGameScreen = () => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue("");
-    Keyboard.dismiss()
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
@@ -50,7 +50,10 @@ const StartGameScreen = () => {
         <Text>You selected </Text>
 
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button  title="START GAME" />
+        <Button
+          title="START GAME"
+          onPress={() => onStartGame(selectedNumber)}
+        />
       </Card>
     );
   }
